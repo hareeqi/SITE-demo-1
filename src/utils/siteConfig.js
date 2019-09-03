5bde73fec1007f07a6f4fe910baa082db6753d76
hareeqi <hareeqi@users.noreply.github.com>
2019-09-04 01:57:38 +0300

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -10,7 +10,7 @@ module.exports = {
   authorUrl: 'https://site-demo2.hareeqi.com/about/', // URL used for author and publisher schema, can be a social profile or other personal site
   userTwitter: '@twitter', // Change for Twitter Cards
   shortTitle: 'Site Demo', // Used for App manifest e.g. Mobile Home Screen
-  shareImage: '/logos/share.jpg', // Open Graph Default Share Image. 1200x1200 is recommended
+  shareImage: '/logos/pif.png', // Open Graph Default Share Image. 1200x1200 is recommended
   shareImageWidth: 900, // Change to the width of your default share image
   shareImageHeight: 600, // Change to the height of your default share image
   siteLogo: '/logos/pif.png', // Logo used for SEO, RSS, and App manifest
