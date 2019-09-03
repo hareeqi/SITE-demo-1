7b4ba4360b43aa609228aa86a116b0acad018e2a
hareeqi <hareeqi@gmail.com>
2019-09-04 00:49:50 +0300

    theme changes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,6 +11,7 @@ const Template = ({ children }) => {
   return (
     <div className="siteRoot">
       <Helmet>
+        <html lang="en" />
         <title>{config.siteTitle}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
